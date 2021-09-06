5414cf2db7c02c6d6089f7fabc63b0ea7674f778
Waleed Malik <wmalik@nextdoor.com>
2021-09-06 18:15:59 -0400

    fixing up some stuff

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
-import requests 
+import requests
+import json
+from typing import NamedTuple
+
 
 app = Flask(__name__, template_folder='.')
 
@@ -7,22 +10,65 @@ HEADERS = {'Content-type':'application/json'}
 RPC_ENDPOINT = 'https://solana-mainnet.phantom.tech'
 LAMPORT_TO_SOL = 0.000000001
 
+class Transaction(NamedTuple):
+    fees: float
+    success: bool
+
+class Resp(NamedTuple):
+    sol_fees: float
+    sol_fees_usd: float
+    fail_count: float
+    fail_cost_usd: float
+    avg_transaction_cost: float
+    num_transactions: float
+    failed: bool
+
+FAILED_RESP = Resp(0,0,0,0,0,0,True)
+
 @app.route("/")
 def index():
-    print('in index')
     return render_template('index.html')
 
 @app.route("/getfees", methods=['GET'])
 def get_fees():
-    addy = request.args.get('addy')
-    print(addy)
-    transactions = request_transactions(addy)
-    print(transactions)
-    total_fees = request_fees(transactions)
-    sol_usd = get_sol_price()
+    try:
+        address = request.args.get('a')
+        sol_usd = get_sol_price()
+   
+        transactions = request_transactions(address)
 
-    gas_spent = total_fees * LAMPORT_TO_SOL * sol_usd
-    return str(gas_spent) + "USD"
+        if(len(transactions) == 0):
+            return json.dumps(FAILED_RESP._asdict())
+
+        fees = request_fees(transactions)
+
+        total_fees_lamports = sum([i.fees for i in fees])
+        total_fees_usd = lamports_to_usd(total_fees_lamports, sol_usd)
+
+        failed_fees = [i.fees for i in fees if not i.success]
+        failed_fees_usd = lamports_to_usd(sum(failed_fees), sol_usd)
+
+        resp = Resp(
+            sol_fees=total_fees_lamports,
+            sol_fees_usd=truncate(total_fees_usd, 4),
+            fail_count=len(failed_fees),
+            fail_cost_usd=truncate(failed_fees_usd, 4),
+            avg_transaction_cost=truncate(total_fees_lamports / len(transactions), 4),
+            num_transactions=len(transactions),
+            failed=False
+        )
+
+        return json.dumps(resp._asdict())
+    except Exception as e:
+        print(e)
+        return json.dumps(FAILED_RESP._asdict())
+
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
+def lamports_to_usd(lamports, sol_usd):
+    return lamports * LAMPORT_TO_SOL * sol_usd
 
 def request_transactions(addy):
     getConfirmedSignaturesForAddress2 = f'''
@@ -59,10 +105,12 @@ def request_fees(transactions):
 
     r = requests.post(RPC_ENDPOINT, data=getConfirmedTransaction, headers=HEADERS)
     for i in r.json():
-        fees.append(i['result']['meta']['fee'])
+        fee = i['result']['meta']['fee']
+        err = i['result']['meta']['err'] == None
+        fees.append(Transaction(fee, err))
 
     print(fees)
-    return sum(fees)
+    return fees
 
 def get_sol_price():
     r = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT')
--- static/sendData.js
+++ static/sendData.js
@@ -1,19 +1,33 @@
-function connectWallet() {    
-    window.solana.connect();
-}
+window.addEventListener('load', async () => {
+    const isPhantomInstalled = window.solana && window.solana.isPhantom
 
-function getAddress(){
-    var addy = window.solana.publicKey.toString();
-    console.log('wallet addy is ' + addy);
+    if (isPhantomInstalled) {
+        window.solana.connect();
+        window.solana.on("connect", () => {
+            var address = window.solana.publicKey.toString();
+            console.log('connected! address is ' + address);
 
-    var xhttp = new XMLHttpRequest();
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var result = JSON.parse(this.responseText);
+                    document.getElementById("sol_fees").innerHTML = result['sol_fees']
+                    document.getElementById("sol_fees_usd").innerHTML = '$' + result['sol_fees_usd']
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
-       }
-    };
+                    document.getElementById("transaction_count").innerHTML = result['num_transactions']
+                    document.getElementById("avg_transaction").innerHTML = result['avg_transaction_cost'] 
 
-    xhttp.open("GET", "getfees?addy=" +addy , true);
-    xhttp.send(addy);    
-}
+                    document.getElementById("fail_count").innerHTML = result['fail_count']
+                    document.getElementById("fail_cost_usd").innerHTML = '$' + result['fail_cost_usd']
+                }
+            };
+    
+            xhttp.open("GET", "getfees?a=" + address, true);
+            xhttp.send(address);
+        })
+    } else {
+        console.log('Sign into Phantom!')
+        $('body').html('<p id="oops">Sign into <strong><a href="https://phantom.app/">Phantom</a></strong>')
+        return;
+    }
+});
